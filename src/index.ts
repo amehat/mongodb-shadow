ff469ab5ea212c77fe3bcb1bc5bb29eefc18c8a0
Arnaud Méhat <amehat@gmail.com>
2022-02-18 17:09:31 +0100

    feat: add export files and types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { default as MockStoreRepository } from './mock-store.repository';
+// eslint-disable-next-line import/prefer-default-export
+export { default as MongoDBShadowModule } from './mongodb-shadow.module';
+export { default as MongoDB } from './mongodb.service';
+export * from './mongodb.type';
+export * from './mongodb/common/mongodb-common.types';
+export * from './mongodb/operations/delete.types';
+export * from './mongodb/operations/update.types';
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,26 @@
+import { ObjectID } from 'bson';
+
 // Typing of a database record
 export type Item = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [key: string]: any;
-  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
 
-  export type Contraint = {
-    field: string;
+export type Contraint = {
+  field: string;
 
-    constraint: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constraint: any;
 
-    operator?: string;
-  };
+  operator?: string;
+};
+
+export type BaseEntity = {
+  _id?: ObjectID;
+};
+
+export type Collection<T> = Set<T>;
+
+export type Collections<T> = Map<string, Collection<T>>;
+
+export type Database<T> = Map<string, Collections<T>>;
