c27be727384b8698363e43f0be4793494236fe27
Arnaud Méhat <amehat@gmail.com>
2022-02-20 17:41:15 +0100

    feat: add insert to mongodb service

--- src/mongodb.service.ts
+++ src/mongodb.service.ts
@@ -3,12 +3,19 @@ import { Injectable } from '@nestjs/common';
 import Store from './store';
 // eslint-disable-next-line import/no-cycle
 import InsertOne from './mongodb/methods/insert-one.method';
-import type { InsertOneResponse } from './mongodb.type';
+// eslint-disable-next-line sort-imports
+import type { InsertOneResponse, InsertResponse } from './mongodb.type';
 import WriteError from './mongodb/common/write-error';
+// eslint-disable-next-line import/no-cycle
+import Insert from './mongodb/methods/insert.method';
 
 @Injectable()
 export default class MongoDB extends Store {
   insertOne<T>(document: T): InsertOneResponse | WriteError {
     return new InsertOne(this).execute<T>(this.databaseName, this.collectionName, document);
+  }
+
+  insert<T>(documents: T[]): InsertResponse | InsertResponse[] | WriteError {
+    return new Insert(this).execute<T>(this.databaseName, this.collectionName, documents);
   }
 }
